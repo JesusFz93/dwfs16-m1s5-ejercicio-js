e41f2f5570f906f9f472d043e870fb32168db60c
Jesus <jesus.fdz93@gmail.com>
2023-02-25 11:27:09 -0600

    ejercicios de js

---
+++ js/codigo.js
@@ -0,0 +1,42 @@
+// let nombre = "Jesus";
+// let apellido = "Fernandez";
+
+// Imprimir valores en la consola
+// console.log(nombre);
+// console.log(apellido);
+
+// Reasignar una variable el valor
+
+/* 
+comnetarios de multiples
+lineas
+*/
+// nombre = "Omar";
+// console.log(nombre);
+
+// Declaracion de una variable
+let variable;
+// Asignacion de una variable
+variable = "algun dato";
+// Declaracion y una asignacion
+let variable2 = "otro dato";
+// Reasignacion de una variable
+variable2 = "reasignacion de dato";
+
+let nombre = "Jesus";
+let apellido = "Fernandez";
+let edad = 30;
+let fechaDeNacimiento = "1993-01-22";
+let soltero = true;
+let activo = false;
+
+// console.log(nombre + " " + apellido);
+// console.log(edad);
+// console.log(fechaDeNacimiento);
+// console.log(soltero);
+
+let precio = "45";
+let cantidad = 5;
+
+let total = Number(precio) + cantidad;
+console.log(total);
