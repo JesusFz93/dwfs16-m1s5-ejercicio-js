9c96dea41a6c70c3f1d78ef6b55b48fe1d4c9aa7
Jesus <jesus.fdz93@gmail.com>
2023-02-25 14:41:58 -0600

    listo

--- js/codigo.js
+++ js/codigo.js
@@ -15,20 +15,20 @@ lineas
 // console.log(nombre);
 
 // Declaracion de una variable
-let variable;
+// let variable;
 // Asignacion de una variable
-variable = "algun dato";
+// variable = "algun dato";
 // Declaracion y una asignacion
-let variable2 = "otro dato";
+// let variable2 = "otro dato";
 // Reasignacion de una variable
-variable2 = "reasignacion de dato";
+// variable2 = "reasignacion de dato";
 
-let nombre = "Jesus";
-let apellido = "Fernandez";
-let edad = 30;
-let fechaDeNacimiento = "1993-01-22";
-let soltero = true;
-let activo = false;
+// let nombre = "Jesus";
+// let apellido = "Fernandez";
+// let edad = 30;
+// let fechaDeNacimiento = "1993-01-22";
+// let soltero = true;
+// let activo = false;
 
 // console.log(nombre + " " + apellido);
 // console.log(edad);
@@ -56,3 +56,100 @@ let activo = false;
 
 // let comparacion = operando1 > operando2 && operando1 < operando2;
 // console.log(comparacion);
+
+// let edad = 9;
+
+// if (edad >= 18) {
+//   console.log("Eres mayor de edad");
+// } else if (edad >= 14) {
+//   console.log("Eres mayor a 14 pero menor a 18");
+// } else if (edad >= 10) {
+//   console.log("Mayor a 10");
+// } else {
+//   console.log("Menor a 10");
+// }
+
+// switch (edad) {
+//   case 7:
+//     console.log("Tu tienes 7");
+//     break;
+//   case 8:
+//     console.log("Tu tienes 8");
+//     break;
+//   default:
+//     console.log("No encontre ningun caso valido");
+//     break;
+// }
+
+// console.log(edad);
+
+// let numero1 = 100;
+// let numero2 = 100;
+
+// if (numero1 > numero2) {
+//   console.log("El numero1 es mayor al numero2");
+// } else if (numero1 < numero2) {
+//   console.log("El numero1 es menor al numero2");
+// } else {
+//   console.log("Los numeros son iguales");
+// }
+
+// console.log("Ejecutado antes  del ciclo for");
+
+// for (let i = 0; i <= 10; i++) {
+//   console.log(i);
+// }
+// console.log("Ejecutado despues del ciclo for");
+
+// let contador = 0;
+// while (contador <= 10) {
+//   console.log(contador);
+
+//   contador++;
+// }
+
+// let numero1 = 100;
+// let numero2 = 50;
+// let numero3 = 8;
+// let numero4 = 9;
+// let numero5 = 76;
+// let numero6 = 45;
+
+// let suma = numero1 + numero2;
+// let suma2 = numero3 + numero4;
+// let suma3 = numero5 + numero6;
+
+// console.log("Suma: " + suma);
+// console.log("Suma2: " + suma2);
+// console.log("suma3: " + suma3);
+
+// function sumaDeNumeros(primerNumero, segundoNumero) {
+//   let suma = primerNumero + segundoNumero;
+//   console.log("Suma: " + suma);
+// }
+
+// function saludar() {
+//   console.log("Hola a todos");
+// }
+
+// sumaDeNumeros(100, 50);
+// sumaDeNumeros(8, 9);
+// sumaDeNumeros(76, 45);
+// saludar();
+
+// Ejercicio
+// function pares(numero) {
+//   for (let i = 1; i <= numero; i++) {
+//     console.log(i * 2);
+//   }
+// }
+
+// pares(3);
+
+// Ejercicio 2
+function nombre(nombre, apellido) {
+  let nombreCompleto = nombre + " " + apellido;
+  console.log(nombreCompleto);
+}
+
+nombre("Roberto", "Rivera");
