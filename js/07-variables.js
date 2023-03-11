d5f892fef3196be7b1555149c9fa1e07be18b19b
Jesus <jesus.fdz93@gmail.com>
2023-03-11 10:10:37 -0600

    string templates

--- js/07-variables.js
+++ js/07-variables.js
@@ -1,3 +1,4 @@
+// Variables
 let nombre = "Jesus";
 
 if (4 < 5) {
@@ -6,3 +7,14 @@ if (4 < 5) {
 }
 
 console.log(nombre);
+
+// String Templates
+let firstName = "Jesus";
+let lastName = "Fernandez";
+let age = 30;
+
+console.log(
+  "Hola soy " + firstName + " " + lastName + ", y tengo " + age + " anios"
+);
+
+console.log(`Hola soy ${firstName} ${lastName}, y tengo ${age} anios`);
